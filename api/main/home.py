1d9456f7b4924a052c1e1f93a30d21031b2a1742
JSnakegitHub <nyekowalter69@gmail.com>
2018-08-28 17:50:01 +0300

    Updated configs

--- api/main/home.py
+++ api/main/home.py
@@ -1,15 +1,19 @@
 from flask import Flask, request, jsonify, make_response, Blueprint
+# from api.users.routes import users
 from werkzeug.security import generate_password_hash, check_password_hash
 from api.answers.models import Answer
 from api.comments.models import Comments
 from api.questions.models import Question
 from api.users.models import User
-from functools import wraps
 import datetime
 import psycopg2
 import psycopg2.extras
 import jwt
+from api.config import configurations
+from functools import wraps
 
+config = configurations()
+config.create_tables()
 
 app = Flask(__name__)
 
