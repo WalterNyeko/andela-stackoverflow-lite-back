5d49ad36991310973883d344b14106e22044bcce
JSnakegitHub <nyekowalter69@gmail.com>
2018-08-31 10:42:53 +0300

    Refactored tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -26,31 +26,12 @@ def test_get_all_questions_is_successfully_rendered(client):
     assert response.status_code == 200
 
 # Testing if the URL for getting one question is accessible
-def test_get_a_question_is_successfully_rendered_for_details_page_with_id(client):
+def test_get_a_question_is_successfully_rendered(client):
     response = client.get('/api/v1/questions/35')
     assert response.status_code == 200
 
-# Tests for POST endpoints. Checking resulting json data:
+# Tests for POST endpoints. Checking resulting json data
 
-# Testing if the answer posted is stored
-def test_post_answer_is_acually_posting(client):
-    response = client.post('/api/v1/questions/35/answers', data={ "answer_body": "Lorem ipsum dolor sit amet"})
-    assert not b'Answer posted successfully' in response.data
-   
-# # Testing if the question posted is really stored
-# def test_post_question_is_actually_posting(client):
-#     response = client.post('/api/v1/questions', data={"question_title": "What is wrong", "question_body" : "Lorem ipsum dolor sit amet"},content_type='application/json')
-#     assert 'Question posted successfully' in response.data
-
-# # Testing if the question being posted is not repeated
-# def test_post_question_is_not_repeated(client):
-#     response = client.post('/api/v1/questions', data={ "question_title": "Title One", "question_body": "Lorem ipsum dolor sit amet"})
-#     assert not 'Question already exists' in response.data
-
-# # Testing if the question being posted is not missing title--users can ignore the body but not title
-# def test_post_question_is_not_missing_title(client):
-#     response = client.post('/api/v1/questions', data={ "question_title": "Title", "question_body": "Lorem ipsum dolor sit amet"})
-#     assert 'Please fill in the title' in response.data
 
 # Testing if the content-type of the page after posting question is application/json or text/html
 def test_post_question_is_application_json_format(client):
--- tests/test_login.py
+++ tests/test_login.py
@@ -5,6 +5,11 @@ from api.connectdb import Configurations
 from api.views import SignUp, valid_email
 from api.questions.models import Question
 from api.answers.models import Answer
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
+from flask import request
 
 config = Configurations()
 questionObject = Question()
@@ -18,17 +23,15 @@ def client(request):
 
     return test_client
 
-def test_question_successfully_inserted_in_the_db(client):
-    response = client.get('/api/v1/questions')
-    assert response.status_code == 200
-
-def test_if_deleted_question_is_completely_deleted(client):
-    response = client.get('/api/v1/questions/24')
-    assert response.status_code == 200
-
 
 def test_if_dbConnection_is_established(client):
     connection = config.connectToDB()
     assert connection is not None
+
+def test_user_provided_username(client):
+    response = client.get('/api/v1/questions/25')
+    assert response.status_code == 200
+
+
 
 
